942450958ee7d0e1d92e739a0593acc7ef3bc4c6
Hannahalfie6 <halfie@udesa.edu.ar>
2026-04-24 18:46:47 -0300

    footer css

--- parcial/src/components/Footer/Footer.js
+++ parcial/src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import "./Footer.css"
 function Footer (){
     return(
         <footer>
+            <p>Trabajo integrador programación III</p>
             <p>Hannah Alfie | Delfina Iribarren | Magdalena Sánchez </p>
         </footer>
     )
