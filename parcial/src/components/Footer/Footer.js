7e84ec5868de5fb9751af907236c4b9bcd5495e7
Hannahalfie6 <halfie@udesa.edu.ar>
2026-04-24 18:41:58 -0300

    footer nuevo

--- parcial/src/components/Footer/Footer.js
+++ parcial/src/components/Footer/Footer.js
@@ -1,22 +1,12 @@
 import { Component } from "react";
 import "./Footer.css"
 
-class Footer extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-
-        }
-    }
-    
-    render(){
-        return(
-        <footer class="alert alert-primary mt-4 text-center">
-            <p class="mb-0">Hannah Alfie | Delfina Iribarren | Magdalena Sánchez </p>
+function Footer (){
+    return(
+        <footer>
+            <p>Hannah Alfie | Delfina Iribarren | Magdalena Sánchez </p>
         </footer>
-        )
-    }
-
-
+    )
 }
+
 export default Footer;
