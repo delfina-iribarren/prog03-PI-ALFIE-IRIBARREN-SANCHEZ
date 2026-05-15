daec55b8c1f73631efdbdbb3a924260e74122781
magui-sanchez <sanchezma@udesa.edu.ar>
2026-05-15 17:06:10 -0300

    hook buscador

--- parcial/src/components/Buscador/Buscador.js
+++ parcial/src/components/Buscador/Buscador.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import "./Buscador.css"
+import{useState} from "react";
 
-class Buscador extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            termino: '',
-            tipo: 'movie'
-        };
+function Buscador(props){
+    const[termino, setTermino] = useState("")
+    const[tipo, setTipo] = useState("")
+    const [busqueda, setBusqueda] = useState("")
+    
+    function controlarCambios(e){
+        setTermino(e.target.value) 
     }
-
-    controlarCambios(e) {
-        this.setState({ termino: e.target.value });
+    function controlarTipo(e){
+        setTipo(e.target.value)
     }
-    controlarTipo(e) {
-        this.setState({tipo: e.target.value})
+    function ejecutarBusqueda(){
+        setBusqueda('/resultados/' + setTermino + "/" + setTipo)
     }
-
-    ejecutarBusqueda(e) {
-        e.preventDefault();
-        this.props.history.push('/resultados/' + this.state.termino + "/" + this.state.tipo);
-    }
-
-    render() {
-        return (
-            <form className="search-form" onSubmit={(e) => this.ejecutarBusqueda(e)}>
-                <input type="text" name="searchData" placeholder="Buscar..." value={this.state.termino} onChange={(e) => this.controlarCambios(e)}/>
-                <select onChange={(e) => this.controlarTipo(e)}>
+    return(
+            <form className="search-form" onSubmit={(e) => setBusqueda(e)}>
+                <input type="text" name="searchData" placeholder="Buscar..." value={setTermino} onChange={(e) => setTermino(e)}/>
+                <select onChange={(e) => setTipo(e)}>
                     <option value="movie">Pelicula</option>
                     <option value="tv">Serie</option>
                 </select>
 
                 <button type="submit" className="btn btn-success btn-sm">Buscar</button> 
             </form>
-        );
-    }
+    )
 }
 
 export default withRouter(Buscador);
